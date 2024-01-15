6ebbec5c0192fa0fcfdb35a88d1d2138a81e03ae
purepay-steve <142355393+purepay-steve@users.noreply.github.com>
2024-01-15 13:30:20 +0100

    feat: added test DI container, merged in logging support

--- src/Pure.Testing.Utilities/TestServicesProvider.cs
+++ src/Pure.Testing.Utilities/TestServicesProvider.cs
@@ -6,21 +6,43 @@
 //  * The MIT License, see https://opensource.org/license/mit/
 
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace Pure.Testing.Utilities;
+namespace Pure.Testing;
 
-public class TestServicesProvider
+public class TestServicesProvider : IServiceProvider
 {
-    public static ILogger<T> GetLogger<T>() where T : class
+    private readonly HostApplicationBuilder _hostBuilder;
+    private readonly IServiceCollection _services;
+    private IHost? _host;
+
+    private IServiceProvider Services => _host?.Services ??
+        throw new InvalidOperationException("Service host not initialised; call Build() before any calls to GetService()");
+
+    public TestServicesProvider()
     {
-        var serviceProvider = new ServiceCollection()
-            .AddLogging()
-            .BuildServiceProvider();
+        _hostBuilder = new HostApplicationBuilder();
+        _services = _hostBuilder.Services;
 
-        var factory = serviceProvider.GetService<ILoggerFactory>();
-
-        return factory?.CreateLogger<T>() ??
-            throw new InvalidOperationException("Unable to create logger");
+        _services.AddLogging();
     }
+
+    public void Build() => _host = _hostBuilder.Build();
+
+    public IServiceCollection AddSingleton<TService>(TService implementationInstance) where TService : class =>
+        _services.AddSingleton(implementationInstance);
+
+    public IServiceCollection AddSingleton<TService, TImplementation>()
+        where TService : class
+        where TImplementation : class, TService =>
+        _services.AddSingleton<TService, TImplementation>();
+
+    public TService? GetService<TService>() => Services.GetService<TService>();
+
+    public object? GetService(Type serviceType) => Services.GetService(serviceType);
+
+    public ILogger<T> GetLogger<T>() where T : class =>
+        Services.GetService<ILoggerFactory>()?.CreateLogger<T>() ??
+            throw new InvalidOperationException("Unable to create logger");
 }
