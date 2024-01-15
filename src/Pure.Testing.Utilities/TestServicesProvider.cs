fe3930e361c1d54585beb314aa01b07ab005c018
purepay-steve <142355393+purepay-steve@users.noreply.github.com>
2024-01-15 11:03:02 +0100

    feat: added logging provider

---
+++ src/Pure.Testing.Utilities/TestServicesProvider.cs
@@ -0,0 +1,26 @@
+﻿//
+// Copyright (c) 2023-2024, Pure Software Ltd.  All rights reserved.
+//
+// Pure Software licenses this file to you under the following license(s):
+//
+//  * The MIT License, see https://opensource.org/license/mit/
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace Pure.Testing.Utilities;
+
+public class TestServicesProvider
+{
+    public static ILogger<T> GetLogger<T>() where T : class
+    {
+        var serviceProvider = new ServiceCollection()
+            .AddLogging()
+            .BuildServiceProvider();
+
+        var factory = serviceProvider.GetService<ILoggerFactory>();
+
+        return factory?.CreateLogger<T>() ??
+            throw new InvalidOperationException("Unable to create logger");
+    }
+}
